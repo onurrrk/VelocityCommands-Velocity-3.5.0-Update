bf412a8aae9225a4a345648a7ce566732f36e3d7
onurr_k <145716470+onurrrk@users.noreply.github.com>
2026-02-23 18:16:51 +0300

    Update ConfigManager.java

--- src/main/java/dev/digitality/velocitycommands/managers/ConfigManager.java
+++ src/main/java/dev/digitality/velocitycommands/managers/ConfigManager.java
@@ -23,37 +23,37 @@ public class ConfigManager {
         CommandOverrideManager.getOverridenCommands().clear();
 
         config = loadConfig("settings.yml");
+        if (config == null) return;
+
         if (!config.contains("disable-tab-complete"))
             config.set("disable-tab-complete", List.of("ver", "version", "icanhasbukkit"));
 
-        ConfigManager.getConfig().getSection("overriden-commands").getKeys().forEach(key -> CommandOverrideManager.getOverridenCommands().put(key.toLowerCase(), ChatUtils.colorize(ConfigManager.getConfig().getString("overriden-commands." + key))));
-        CommandOverrideManager.getDisabledCommands().addAll(ConfigManager.getConfig().getStringList("disabled-commands").stream().map(String::toLowerCase).toList());
-        CommandOverrideManager.getDisabledAutocompleteCommands().addAll(ConfigManager.getConfig().getStringList("disable-tab-complete").stream().map(String::toLowerCase).toList());
+        config.getSection("overriden-commands").getKeys().forEach(key -> 
+            CommandOverrideManager.getOverridenCommands().put(key.toLowerCase(), 
+            ChatUtils.colorize(config.getString("overriden-commands." + key))));
+        
+        CommandOverrideManager.getDisabledCommands().addAll(config.getStringList("disabled-commands").stream().map(String::toLowerCase).toList());
+        CommandOverrideManager.getDisabledAutocompleteCommands().addAll(config.getStringList("disable-tab-complete").stream().map(String::toLowerCase).toList());
     }
 
-    @Subscribe
     public static Configuration loadConfig(String fileName) {
-        if (!DigitalMain.getInstance().getDataDirectory().toFile().exists()) {
-            DigitalMain.getInstance().getDataDirectory().toFile().mkdirs();
+        File dataFolder = DigitalMain.getInstance().getDataDirectory().toFile();
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
         }
 
-        File file = new File(DigitalMain.getInstance().getDataDirectory().toFile(), fileName);
-        // Create default config
+        File file = new File(dataFolder, fileName);
         if (!file.exists()) {
-            file.getParentFile().mkdirs();
-
             try (InputStream in = ConfigManager.class.getResourceAsStream("/" + fileName)) {
-                if (in != null)
-                    Files.copy(in, file.toPath());
-                else
-                    Files.createFile(file.toPath());
+                if (in != null) Files.copy(in, file.toPath());
+                else Files.createFile(file.toPath());
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
         try {
-            return ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(DigitalMain.getInstance().getDataDirectory().toFile(), fileName));
+            return ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -62,10 +62,10 @@ public class ConfigManager {
 
     public static void saveConfig(Configuration configuration, String fileName) {
         try {
-            ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, new File(DigitalMain.getInstance().getDataDirectory().toFile(), fileName));
+            File file = new File(DigitalMain.getInstance().getDataDirectory().toFile(), fileName);
+            ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, file);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
-
